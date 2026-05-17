14b07b1456eba03eb9cfc45382bcea895a40acda
Sammy <bhagisamaksh@gmail.com>
2026-05-17 14:46:03 +0530

    Added secret key

--- backend/config.py
+++ backend/config.py
@@ -1,12 +1,12 @@
 import os
 DIR = os.path.abspath(os.path.dirname(__file__))
 class Config:
-    #SECRET_KEY = "abcd12345"
+    SECRET_KEY = os.getenv("SECRET_KEY")
     JWT_EXPIRATION_SECONDS = 3000
     database_url = os.getenv("DATABASE_URL")
     if database_url and database_url.startswith("postgres://"):
         database_url = database_url.replace("postgres://", "postgresql://", 1)
     SQLALCHEMY_DATABASE_URI = database_url or "sqlite:///" + os.path.join(DIR, "instance","placement.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    #EMAIL_USER = "samaksh.bhagi.dev@gmail.com"
-    #EMAIL_PASSWORD = "xowumjoywlvqmhtz"
+    EMAIL_USER = os.getenv("EMAIL_USER")
+    EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
